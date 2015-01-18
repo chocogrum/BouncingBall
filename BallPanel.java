b8072e1cf0913fccdd253c9bd87f65011cf093fc
Graham Thomas <pretzelogic@gmail.com>
2015-01-18 15:15:22 +0000

    Introduced Timer

--- BallPanel.java
+++ BallPanel.java
@@ -3,14 +3,26 @@ import java.awt.Graphics;
 import java.awt.geom.Ellipse2D;
 import java.awt.Color;
 import java.awt.Graphics2D;
-import javax.swing.Timer; 
+import javax.swing.Timer;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class BallPanel extends JPanel
 {
 
+	private Timer ballTimer;
+	private int ballSpeed;
+	private int ballX;
+	private int ballY;
+	private boolean forwards;
+
 	public BallPanel()
 	{
-	
+		ballSpeed = 5;
+		ballTimer = new Timer( ballSpeed, new BallTimerHandler() );
+		ballX = 5;
+		ballY = 5;
+		forwards = true;
 	}
 	
 	public void paintComponent( Graphics g )
@@ -19,6 +31,34 @@ public class BallPanel extends JPanel
 		Graphics2D g2d = ( Graphics2D ) g;
 		
 		g2d.setPaint( Color.RED );
-		g2d.fill( new Ellipse2D.Double( 5, 5, 100, 100 ) );
+		g2d.fill( new Ellipse2D.Double( ballX, ballY, 100, 100 ) );
+		if( forwards )
+		{
+			if( ballX == 500 )
+			{
+				ballX--;
+				forwards = false;
+			}
+			else
+				ballX++;
+		}
+		else
+		{
+			if( ballX == 0 )
+			{
+				ballX++;
+				forwards = true;
+			}
+			else
+				ballX--;
+		}
+	}
+	
+	private class BallTimerHandler implements ActionListener
+	{
+		public void actionPerformed( ActionEvent actionEvent )
+		{
+			repaint();
+		}
 	}
 }
